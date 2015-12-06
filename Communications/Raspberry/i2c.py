a669f2c0d73e9840d72894251ee24a85e3931db3
Tito1337 <tito@webtito.be>
2015-12-06 17:00:56 +0000

    I2C sachant décoder Gcode Q1 et Q2

--- Communications/Raspberry/i2c.py
+++ Communications/Raspberry/i2c.py
@@ -5,13 +5,12 @@ from quick2wire.i2c import I2CMaster, writing, reading
 
 address = 0x08
 
-def i2c_send(str):
-	with I2CMaster() as bus:
+with I2CMaster() as bus:
+	def i2c_send(str):
 		bus.transaction(writing(address, [ord(c) for c in str]))
 
-def i2c_receive(size):
-	with I2CMaster() as bus:
+	def i2c_receive(size):
 		return bus.transaction(reading(address, size))
 
-i2c_send("hello")
-print(i2c_receive(10))
+	i2c_send("Q2 A? C?\n")
+	print(i2c_receive(31))
