cbc317a43982e447866f953e42c8ae97bfaa41ce
GrInGo04 <obdcme_benua@hotmail.com>
2016-05-08 20:45:22 +0200

    Added files via upload

---
+++ Moteurs-Communications/Arduino/Moteurs-CommunicationVERSION_FINALE/Moteurs-CommunicationVERSION_FINALE.ino
@@ -0,0 +1,13 @@
+#include <EEPROM.h>
+int i;
+void setup() {
+  Communications_setup();
+  Moteurs_setup();
+
+  
+  doMove(212,0); //54 cm = un tour, la première variable envois une distance linéraire à parcourir, et la deuxième un angle.
+}
+
+void loop() {
+    Moteurs_loop();
+}
